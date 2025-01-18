9b3d5657d6a1cd36728db493d6ee45f9311356be
Aayushi Jain <iamaayushirocks@gmail.com>
2025-01-18 22:14:16 +0530

    Routes changed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
          <BrowserRouter>
 
          <Routes>
-             <Route path="/home" element={<Home />} />
+             <Route path="/" element={<Home />} />
              
 
              
